6eba456d51e4bbdfcdb8e3bba0f4135be6e314b8
gibbonmi <gibs.mikej@gmail.com>
2025-06-07 09:45:51 -0400

    fix the tests

--- internal/auth/get_api_key_test.go
+++ internal/auth/get_api_key_test.go
@@ -24,7 +24,7 @@ func TestGetAPIKey(t *testing.T) {
 		{
 			key:       "Authorization",
 			value:     "-",
-			expectErr: "malformed authorizationheader",
+			expectErr: "malformed authorization header",
 		},
 		{
 			key:       "Authorization",
